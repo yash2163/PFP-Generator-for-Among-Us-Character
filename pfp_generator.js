66abf693031a4425d6204b3cc662bd3fdf486483
yash2163 <yashrajput2163@gmail.com>
2024-07-11 13:40:10 +0530

    pushing all the files to github

---
+++ pfp_generator.js
@@ -0,0 +1,24 @@
+// function addpic(){
+//     var x=document.createElement("IMG");
+//     x.setAttribute("src","/Users/yashrajput/Desktop/pfp generator/face-1.webp");
+//     x.setAttribute("width","20");
+//     // x.setAttribute("height","20");
+//     x.setAttribute("alt","face");
+//     document.getElementById("face-place").innerHTML=x;
+//     // document.body.appendChild(x);
+//     // document.getElementById('div-4').appendChile(x);
+
+// }
+
+// var x=document.getElementById('add-pic');
+// x.addEventListener("click",myfunction);
+// function myfunction(){
+//     var x=document.createElement("IMG");
+//     x.setAttribute("src","/Users/yashrajput/Desktop/sem-5 WebDevelopment/face-1 copy.webp");
+//     x.setAttribute("width","304");
+//     x.setAttribute("alt","the face");
+//     document.getElementById("face-space").appendChild(x);
+// }
+//             // document.getElementById("face-space").innerHTML=x;
+//             // document.body.appendChild(x);
+// }
